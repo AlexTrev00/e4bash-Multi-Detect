0a8aee0e7a0cf77c90d654155f3f574a9f9492ed
Zeus099 <89210446+Zeus099@users.noreply.github.com>
2021-11-10 17:15:41 -0600

    execute file
    execute file in linux

---
+++ e4Bash multi-detect.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if type -t wevtutil &> /dev/null
+then
+	OS=MSWin
+elif type -t scutil &> /dev/null
+then
+	OS=MacOs
+else
+	OS=Linux
+fi
+echo $OS
+function is_alive_ping(){
+	ping -c 1 $1 > /dev/null 2>&1
+	[$? -eq 0] && IPS=$i
+}
+for i in 192.168.1.{1..99}
+do
+	is_alive_ping $i & disown
+
+
+done
+firstport=10
+lastport=500
+for $i in "${IPS[@]}"
+do
+	counter=$firstport; counter<=$lastport; counter++
+	echo "$i $counter"
+done
